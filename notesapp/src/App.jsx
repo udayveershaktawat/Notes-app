77141c95027bfb913f92bd5a9a899eea99dc0621
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-07 23:31:22 +0530

    first page ready

--- notesapp/src/App.jsx
+++ notesapp/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="h-screen  bg-black text-white">
+      <form className="flex items-start flex-col gap-4  p-10 " action="">
+        <input type="text" 
+        placeholder="Enter Notes Task " 
+        className="px-5 py-2 w-1/2 border-2 rounded"
+        
+        />
+        <input type="text" placeholder="Write Details " 
+        className="px-5 h-20 w-1/2   py-2 border-2 rounded"
+        
+        />
+        <button className="bg-white w-1/2  text-black px-5 py-2 rounded">Add Note</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
