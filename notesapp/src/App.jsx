80e88192b89b6479f225b7116b9a5c1c7ad32695
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-07 23:40:56 +0530

    completed front page

--- notesapp/src/App.jsx
+++ notesapp/src/App.jsx
@@ -6,14 +6,14 @@ const App = () => {
       <form className="flex items-start flex-col gap-4  p-10 " action="">
         <input type="text" 
         placeholder="Enter Notes Task " 
-        className="px-5 py-2 w-1/2 border-2 rounded"
+        className="px-5 py-2 w-1/2 border-2 font-medium rounded outline-none"
         
         />
-        <input type="text" placeholder="Write Details " 
-        className="px-5 h-20 w-1/2   py-2 border-2 rounded"
+        <textarea type="text" placeholder="Write Details " 
+        className="px-5 h-32 w-1/2 font-medium  py-2 border-2 rounded outline-none"
         
         />
-        <button className="bg-white w-1/2  text-black px-5 py-2 rounded">Add Note</button>
+        <button className="bg-white w-1/2 font-medium text-black px-5 py-2 rounded outline-none">Add Note</button>
       </form>
     </div>
   );
